03bc67e420add132c5f79a3f768bc7cbdb5855ce
Marcio Freire Silverio <mfsilverio@ymail.com>
2025-07-17 18:49:39 -0300

    Update settings.py

--- catalogo_web/settings.py
+++ catalogo_web/settings.py
@@ -113,12 +113,12 @@ MEDIA_URL = '/media/'
 
 # Cloudinary via django-storages
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.cloudinary.CloudinaryStorage'
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': config('CLOUDINARY_API_KEY'),
-    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
-
 # Render exige domínio confiável para CSRF
 RENDER_HOST = config('RENDER_EXTERNAL_HOSTNAME', default='')
 
