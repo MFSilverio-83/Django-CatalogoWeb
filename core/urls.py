17cb9014ba244d416386db40b3652a317c57d8df
Marcio Freire Silverio <mfsilverio@ymail.com>
2025-08-09 16:22:38 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'), # link para homepage do site
     path('<slug:category_slug>/', CategoriaProdutoView.as_view(), name='produtos_categorias'),
-
+    path('produto/<int:pk>/upload-imagens/', MultiImageUploadView.as_view(), name='upload_multiplas_imagens'),
 ]
