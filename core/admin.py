617570c5131c31086fc9e967470fa5efb8b387d7
Marcio Freire Silverio <mfsilverio@ymail.com>
2025-08-09 16:23:45 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Categoria, Produto, ImagemProduto
 
 # Esta classe permite adicionar imagens DIRETAMENTE na página de cadastro do produto
@@ -19,7 +22,7 @@ class CategoriaAdmin(admin.ModelAdmin):
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
     # Campos que aparecerão na listagem de produtos
-    list_display = ('nome', 'categoria', 'preco_formatado', 'ativo')
+    list_display = ('nome', 'categoria', 'preco_formatado', 'ativo', 'link_upload_imagens')
     # Permite editar o campo 'ativo' diretamente na listagem (muito prático!)
     list_editable = ('ativo',)
     # Adiciona filtros na lateral direita para facilitar a busca
@@ -29,5 +32,21 @@ class ProdutoAdmin(admin.ModelAdmin):
     # Adiciona o formulário de Imagens (que criamos acima) dentro da página do produto
     inlines = [ImagemProdutoInline]
 
-# Não precisamos registrar ImagemProduto separadamente, pois já está como 'inline'
-# admin.site.register(ImagemProduto)
+    # Campo para exibir o link no formulário de edição do produto
+    readonly_fields = ('link_upload_imagens',)
+
+    def link_upload_imagens(self, obj):
+        if obj.pk:
+            url = reverse('upload_multiplas_imagens', args=[obj.pk])
+            return format_html('<a class="button" href="{}">Adicionar várias fotos</a>', url)
+        return ""
+
+    link_upload_imagens.short_description = "Adicionar Fotos"
+
+    # NOVO MÉTODO ADICIONADO AQUI
+    def response_add(self, request, obj, post_response_object=None):
+        """Redireciona para a página de edição do objeto após a criação."""
+        # Cria a URL para a página de edição do produto recém-criado
+        url = reverse('admin:core_produto_change', args=[obj.pk])
+        # Redireciona para essa URL
+        return redirect(url)
