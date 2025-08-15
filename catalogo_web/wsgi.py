9911ecb4ff3cb7d61fb35f384508830845ca94af
Marcio Freire Silverio <mfsilverio@ymail.com>
2025-08-15 10:33:06 -0300

    Update wsgi.py

--- catalogo_web/wsgi.py
+++ catalogo_web/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catalogo_web.settings')
 
 application = get_wsgi_application()
+
+try:
+    import post_deploy
+except Exception as e:
+    print(f"⚠️ Erro ao rodar post_deploy: {e}")
